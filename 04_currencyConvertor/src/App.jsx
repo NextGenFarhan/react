d1b8a82b63670b567e18f705645e8753b42d9e7d
NextGenFarhan <farhannnkhan770&gmail.com>
2025-12-30 09:59:48 +0530

    calci

--- 04_currencyConvertor/src/App.jsx
+++ 04_currencyConvertor/src/App.jsx
@@ -25,14 +25,12 @@ function App() {
   }
 
   return (
-
     <div
       className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
       style={{
         backgroundImage: `url('https://images.pexels.com/photos/159888/pexels-photo-159888.jpeg')`,
       }}
     >
-    
     
       <div className="w-full">
         <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
